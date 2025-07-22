71c229bad21337d4d617c95772e2c65a97251e7e
Matteo <matteo.monari6@studio.unibo.it>
2025-07-22 17:36:36 +0200

    edit CombatNodel

--- src/main/java/it/unibo/progetto_oop/Combat/MVC_Pattern/CombatModel.java
+++ src/main/java/it/unibo/progetto_oop/Combat/MVC_Pattern/CombatModel.java
@@ -1,5 +1,15 @@
 package it.unibo.progetto_oop.Combat.MVC_Pattern;
 
 public class CombatModel {
+
+    private int size;
+    private Position playerPosition;
+    private Position enemyPosition;
+    private Position attackPosition;
+
+    private int playerHealth;
+    private int enemyHealth;
+    private final int maxHealth = 100;
+
     
 }
--- src/main/java/it/unibo/progetto_oop/Combat/MVC_Pattern/Position/Position.java
+++
@@ -1,3 +0,0 @@
-package main.java.it.unibo.progetto_oop.Combat.MVC_Pattern.Position;
-
-public record Position(int x, int y) {}
