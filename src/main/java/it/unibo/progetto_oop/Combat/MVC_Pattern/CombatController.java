dc3ee9d208a1327227e25c8af06474bd80afd48a
Kelly <kelly.applebee@studio.unibo.it>
2025-07-22 16:47:15 +0200

    feat: Creats CombatController

--- src/main/java/it/unibo/progetto_oop/Combat/MVC_Pattern/CombatController.java
+++ src/main/java/it/unibo/progetto_oop/Combat/MVC_Pattern/CombatController.java
@@ -1,5 +1,14 @@
 package it.unibo.progetto_oop.Combat.MVC_Pattern;
 
 public class CombatController {
-    
+    private CombatModel model;
+    private CombatView view;
+
+    public CombatController(CombatModel model, CombatView view){
+        this.model = model;
+        this.view = view;
+        view.createGrid(model.getCells());
+        view.setVisible(true);
+    }
+
 }
