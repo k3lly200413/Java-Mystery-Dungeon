e9c7b838a3734605f066dbba1dc38b905e9ea620
Kelly <kelly.applebee@studio.unibo.it>
2025-07-22 16:17:57 +0200

    feat: fixes Main to be aligned with new View

--- src/main/java/it/unibo/progetto_oop/Combat/Main.java
+++ src/main/java/it/unibo/progetto_oop/Combat/Main.java
@@ -1,9 +1,13 @@
 package it.unibo.progetto_oop.Combat;
 
+import it.unibo.progetto_oop.Combat.MVC_Pattern.CombatController;
+import it.unibo.progetto_oop.Combat.MVC_Pattern.CombatModel;
+import it.unibo.progetto_oop.Combat.MVC_Pattern.CombatView;
+
 public class Main {
     public static void main(String[] args){
         CombatModel model = new CombatModel();
-        CombatView view = new CombatView();
+        CombatView view = new CombatView(12);
         CombatController controller = new CombatController();
     }
 }
