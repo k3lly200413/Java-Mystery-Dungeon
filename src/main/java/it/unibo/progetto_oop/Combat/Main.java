35f2c1801c2b036b56a08a79620e79a11551843b
Kelly Applebee <kelly.applebee@studio.unibo.it>
2025-07-23 16:21:09 +0200

    feat: change main to be in line with Controller

--- src/main/java/it/unibo/progetto_oop/Combat/Main.java
+++ src/main/java/it/unibo/progetto_oop/Combat/Main.java
@@ -8,6 +8,6 @@ public class Main {
     public static void main(String[] args){
         CombatModel model = new CombatModel();
         CombatView view = new CombatView(12);
-        CombatController controller = new CombatController();
+        CombatController controller = new CombatController(model, view);
     }
 }
