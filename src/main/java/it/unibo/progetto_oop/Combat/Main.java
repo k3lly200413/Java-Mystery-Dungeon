883ce294ef1008c8fa80d38a304bce8ec9290fb7
Matteo <matteo.monari6@studio.unibo.it>
2025-07-23 20:37:16 +0200

    example main

--- src/main/java/it/unibo/progetto_oop/Combat/Main.java
+++ src/main/java/it/unibo/progetto_oop/Combat/Main.java
@@ -6,7 +6,16 @@ import it.unibo.progetto_oop.Combat.MVC_Pattern.CombatView;
 
 public class Main {
     public static void main(String[] args){
-        CombatModel model = new CombatModel();
+
+        //es
+        int size = 15;
+        int playerPower = 5;
+        int playerPoisonPower = 2;
+        int enemyPower = 3;
+        int enemySpeed = 3;
+        String enemyName = "Dragon";
+        
+        CombatModel model = new CombatModel(size, playerPower, playerPoisonPower, enemyPower, enemySpeed, enemyName);
         CombatView view = new CombatView(12);
         CombatController controller = new CombatController(model, view);
     }
