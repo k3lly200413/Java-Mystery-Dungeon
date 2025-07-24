c0fcaf63903000faa80ccb35f646d99963fd4609
Kelly Applebee <kelly.applebee@studio.unibo.it>
2025-07-24 14:41:26 +0200

    refactor: Add javadoc to controller

--- src/main/java/it/unibo/progetto_oop/Combat/MVC_Pattern/CombatController.java
+++ src/main/java/it/unibo/progetto_oop/Combat/MVC_Pattern/CombatController.java
@@ -4,10 +4,24 @@ import it.unibo.progetto_oop.Combat.Position.Position;
 
 // import javax.swing.Timer;
 
+/**
+ * Controller class in Model View Controller Pattern
+ * 
+ * @author Kelly.applebee@studio.unibo.it
+ */
 public class CombatController {
     private CombatModel model;
     private CombatView view;
 
+    /**
+     * Contructor of CombatController takes in both model and view
+     * <p>
+     * @param model Model which holds information necessary to controller
+     * @param view  View which displays on screen information taken from controlller
+     * 
+     * 
+     * @author kelly.applebee@studio.unibo.it
+     */
     public CombatController(CombatModel model, CombatView view){
         this.model = model;
         this.view = view;
@@ -15,6 +29,12 @@ public class CombatController {
         view.setVisible(true);
     }
 
+    /**
+     * Default method to redraw View 
+     * 
+     * 
+     * @author kelly.applebee@studio.unibo.itc
+     */
     private void redrawView(){
         view.redrawGrid(model.getPlayerPosition(), model.getEnemyPosition(), new Position(0, 0), true, true, false, false, 1, 1);
     }
