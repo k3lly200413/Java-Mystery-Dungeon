0d711952ef6927e0195ee7dfb98c2d1bb6e0ea0e
Kelly Applebee <kelly.applebee@studio.unibo.it>
2025-07-30 17:28:42 +0200

    Created PotionStrategy Interface

---
+++ src/main/java/it/unibo/progetto_oop/Combat/PotionStrategy/CurePoison.java
@@ -0,0 +1,12 @@
+package it.unibo.progetto_oop.Combat.PotionStrategy;
+
+import it.unibo.progetto_oop.Combat.MVC_Pattern.CombatModel;
+
+public class CurePoison implements PotionStrategy{
+
+    @Override
+    public void applyEffect(CombatModel user) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'applyEffect'");
+    }
+}
