64abeb5351ec257fd50a887db9cdf1e72e467781
L0vli <laura.bertozzi5@studio.unibo.it>
2025-07-31 22:37:12 +0200

    feat: add first files for overworld

---
+++ src/main/java/it/unibo/progetto_oop/Overworld/Inventory/Item.java
@@ -0,0 +1,5 @@
+package it.unibo.progetto_oop.Overworld.Inventory;
+
+public class Item {
+    
+}
---
+++ src/main/java/it/unibo/progetto_oop/Overworld/PlayerObserver/PlayerObserver.java
@@ -0,0 +1,7 @@
+package it.unibo.progetto_oop.Overworld.PlayerObserver;
+
+import it.unibo.progetto_oop.Overworld.Inventory.Inventory;
+
+// Interface for objects that want to observe the Player
+public interface PlayerObserver {
+}
