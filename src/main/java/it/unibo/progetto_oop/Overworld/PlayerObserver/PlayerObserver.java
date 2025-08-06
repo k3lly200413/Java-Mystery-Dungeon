cafd209a4b07f428cc42e9b77940223c998c9171
Laura Bertozzi <laura.bertozzi5@studio.unibo.it>
2025-08-06 12:36:19 +0200

    moved playerObserver inside player

--- src/main/java/it/unibo/progetto_oop/Overworld/PlayerObserver/PlayerObserver.java
+++
@@ -1,7 +0,0 @@
-package it.unibo.progetto_oop.Overworld.PlayerObserver;
-
-import it.unibo.progetto_oop.Overworld.Inventory.Inventory;
-
-// Interface for objects that want to observe the Player
-public interface PlayerObserver {
-}
