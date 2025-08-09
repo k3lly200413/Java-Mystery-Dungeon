ba69c3d6bc79c7f38126799377ce071309ae61cc
Laura Bertozzi <laura.bertozzi5@studio.unibo.it>
2025-08-09 10:37:44 +0200

    created Potion and changed Adapter_Pattern name

--- src/main/java/it/unibo/progetto_oop/Overworld/AdapterPattern/OverworldPlayerAdapter.java
+++ src/main/java/it/unibo/progetto_oop/Overworld/AdapterPattern/OverworldPlayerAdapter.java
@@ -1,4 +1,4 @@
-package it.unibo.progetto_oop.Overworld.Adapter_Pattern;
+package it.unibo.progetto_oop.Overworld.AdapterPattern;
 
 import it.unibo.progetto_oop.Overworld.Player.Player;
 
---
+++ src/main/java/it/unibo/progetto_oop/Overworld/PotionStrategy/Potion.java
@@ -0,0 +1,15 @@
+package it.unibo.progetto_oop.Overworld.PotionStrategy;
+
+import it.unibo.progetto_oop.Overworld.Inventory.Item;
+// import it.unibo.progetto_oop.Overworld.AdapterPattern.PossibleUser;
+import it.unibo.progetto_oop.Combat.Position.Position;
+
+public class Potion extends Item{
+
+    Potion( String name, String description, Position position) {
+        super(name, description, position);
+    }
+
+    // TODO: implement use method
+    // TODO: add strategy
+}
