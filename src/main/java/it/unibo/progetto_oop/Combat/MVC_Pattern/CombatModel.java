dfd2f2cdd75f08fa7ecd7167f9b1a9d1e138ce47
Matteo Monari <matteo.monari6@studio.unibo.it>
2025-08-13 16:31:08 +0200

    feat: enhance CombatModel with stamina management

--- src/main/java/it/unibo/progetto_oop/Combat/MVC_Pattern/CombatModel.java
+++ src/main/java/it/unibo/progetto_oop/Combat/MVC_Pattern/CombatModel.java
@@ -1,4 +1,5 @@
 package it.unibo.progetto_oop.Combat.MVC_Pattern;
+import java.util.ArrayList;
 import java.util.Objects;
 
 import it.unibo.progetto_oop.Combat.Position.Position;
@@ -14,37 +15,70 @@ public class CombatModel implements PossibleUser{
     private int playerHealth;
     private int enemyHealth;
     private final int maxHealth = 100;
-    private int buff;
+
+    private int playerStamina;
+    private int playerStaminaMax = 100;
 
     private int playerPower;
     private int playerPoisonPower;
+    private int enemyPoisonPower;
+    private int playerLongRangePower;
+    private int enemyLongRangePower;
     private int enemyPower;
-    private int enemySpeed; // If needed for future logic
-    private String enemyName; // If needed
+    private final int enemySpeed; // If needed for future logic
+    private final String enemyName; // If needed
 
     private boolean enemyPoisoned;
     private boolean isPlayerPoison;
     private boolean isPlayerTurn = true; // Added to manage turns
+    private final int basicPlayerPower = 1;
+    private Position whoDied;
 
-    public CombatModel(int size, int playerPower, int playerPoisonPower,
-            int enemyPower, int enemySpeed, String enemyName) {
+    private boolean isBossTurn;
+    private int bossAttackCounter = 0;
+    private int maxBossHit = 3;
 
+    private String currentBossState = "NORMAL";
+    private ArrayList<Position> deathRayPath = new ArrayList<>();
+    private int bossTurnCounter = 0;
+
+    private boolean poisonAnimation;
+
+
+
+    public CombatModel(int size, Position playerPosition, Position enemyPosition, Position attackPosition,
+            int playerHealth, int enemyHealth, int playerStamina, int playerStaminaMax, int playerPower,
+            int playerPoisonPower, int enemyPoisonPower, int playerLongRangePower, int enemyLongRangePower,
+            int enemyPower, int enemySpeed, String enemyName, boolean enemyPoisoned, boolean isPlayerPoison,
+            boolean isPlayerTurn, Position whoDied, boolean isBossTurn, int bossAttackCounter, int maxBossHit,
+            String currentBossState, ArrayList<Position> deathRayPath, int bossTurnCounter, boolean poisonAnimation) {
         this.size = size;
+        this.playerPosition = playerPosition;
+        this.enemyPosition = enemyPosition;
+        this.attackPosition = attackPosition;
+        this.playerHealth = playerHealth;
+        this.enemyHealth = enemyHealth;
+        this.playerStamina = playerStamina;
+        this.playerStaminaMax = playerStaminaMax;
         this.playerPower = playerPower;
-        this.buff = 0;
         this.playerPoisonPower = playerPoisonPower;
+        this.enemyPoisonPower = enemyPoisonPower;
+        this.playerLongRangePower = playerLongRangePower;
+        this.enemyLongRangePower = enemyLongRangePower;
         this.enemyPower = enemyPower;
         this.enemySpeed = enemySpeed;
         this.enemyName = enemyName;
-
-        this.playerHealth = maxHealth;
-        this.enemyHealth = maxHealth;
-        this.enemyPoisoned = false;
-
-        //set start positions
-        this.playerPosition = new Position((this.size / 3) - 2, (this.size / 2));  // Player starts on the left side
-        this.enemyPosition = new Position(this.size - ((this.size / 3) - 1), (this.size / 2));  // Enemy starts on the right side
-        this.attackPosition = new Position(0, 0);
+        this.enemyPoisoned = enemyPoisoned;
+        this.isPlayerPoison = isPlayerPoison;
+        this.isPlayerTurn = isPlayerTurn;
+        this.whoDied = whoDied;
+        this.isBossTurn = isBossTurn;
+        this.bossAttackCounter = bossAttackCounter;
+        this.maxBossHit = maxBossHit;
+        this.currentBossState = currentBossState;
+        this.deathRayPath = deathRayPath;
+        this.bossTurnCounter = bossTurnCounter;
+        this.poisonAnimation = poisonAnimation;
     }
 
     //getters
@@ -143,11 +177,33 @@ public class CombatModel implements PossibleUser{
 
     public void increasePlayerPower(int power){
         this.playerPower += power;
-        this.buff = power; // the increased power is stored in buff
+    }
+
+    public void increaseEnemyPower(int power){
+        this.enemyPower += power;
+    }
+
+    public void resetPlayerPower() {
+        this.playerPower = this.basicPlayerPower; // Reset player power to basic value
     }
 
     public void decreasePlayerHealth(int amount) {
         this.playerHealth = Math.max(0, this.playerHealth - amount);  //only decrease health, do not allow negative values
+    }
+
+    public void increasePlayerMaxStamina(int amount) {
+        this.playerStaminaMax += amount;
+    }
+
+    public void decreasePlayerMaxStamina(int amount) {
+        this.playerStaminaMax = Math.max(0, (this.playerStaminaMax - amount));
+    }
+    public void decreasePlayerStamina(int amount) {
+        this.playerStamina = Math.max(0, this.playerStamina - amount);  //only decrease stamina, do not allow negative values
+    }
+
+    public void increasePlayerStamina(int amount) {
+        this.playerStamina= Math.min(this.playerStaminaMax, (this.playerStamina + amount));
     }
 
     public void decreaseEnemyHealth(int amount) {
@@ -155,11 +211,42 @@ public class CombatModel implements PossibleUser{
     }
     
     public boolean isGameOver() {
-        return playerHealth <= 0 || enemyHealth <= 0;
+        if(this.playerHealth <= 0){
+            this.whoDied = this.getPlayerPosition();
+            return true;
+        }else if(this.enemyHealth <= 0){
+            this.whoDied = this.getEnemyPosition();
+            return true;
+        }
+        return false;
     }
 
     public void setPlayerPoisoned(boolean isPoisoned) {
-        this.isPlayerPoison = true;
+        this.isPlayerPoison = isPoisoned;
     }
+
+    public void clearNossAttackCount(){
+        this.bossAttackCounter = 0;
+    }
+
+    public void increaseBossAttackCounter() {
+        this.bossAttackCounter++;
+    }
+    public void increaseBossTurnCounter() {
+        this.bossTurnCounter++;
+    }
+    
+    public void resetBossTurnCounter() {
+        this.bossTurnCounter = 0;
+    }
+
+    public void addDeathRayPosition(Position nextPosition) {
+        this.deathRayPath.add(nextPosition);
+    }
+
+    public void clearDeathRayPath() {
+        this.deathRayPath.clear();
+    }
+    
 
 }
