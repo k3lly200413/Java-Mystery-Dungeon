c5121185c82b4d1ef2b651fbd74d70fb81985b09
Laura Bertozzi <laura.bertozzi5@studio.unibo.it>
2025-08-16 14:39:52 +0200

    feat: added OverworldController and written OverworldModel

---
+++ src/main/java/it/unibo/progetto_oop/Overworld/MVC/OverworldController.java
@@ -0,0 +1,5 @@
+package it.unibo.progetto_oop.Overworld.MVC;
+
+public class OverworldController {
+    
+}
--- src/main/java/it/unibo/progetto_oop/Overworld/MVC/OverworldModel.java
+++ src/main/java/it/unibo/progetto_oop/Overworld/MVC/OverworldModel.java
@@ -1,5 +1,50 @@
 package it.unibo.progetto_oop.Overworld.MVC;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import it.unibo.progetto_oop.Combat.Inventory.Inventory;
+import it.unibo.progetto_oop.Combat.Inventory.Item;
+import it.unibo.progetto_oop.Overworld.Player.Player;
+
 public class OverworldModel {
+    private Player player;
+    private List<Item> items = new ArrayList<>();
+    private Inventory inventory;
+    
+
+    public OverworldModel(Player player, List<Item> items, Inventory inventory){
+        this.player = player;
+        this.items = items;
+        this.inventory = inventory;
+    }
+
+    // --- getter methods ---
+
+    /**
+     * 
+     * @return Posizione del giocatore
+     */
+    public Player getPlayer(){
+        return this.player;
+    }
+
+    /**
+     * 
+     * @return the list of items in the overworld
+     */
+    public List<Item> getItem(){
+        return this.items;
+    }
+
+
+    /**
+     * 
+     * @return the player's inventory
+     */
+
+    public Inventory getInventoryInstance(){
+        return this.inventory;
+    }
     
 }
