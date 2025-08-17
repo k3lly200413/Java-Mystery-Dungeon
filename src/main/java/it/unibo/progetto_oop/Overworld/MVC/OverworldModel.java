5094a6f127242ff8cba9973a62101361a9c837b8
Laura Bertozzi <laura.bertozzi5@studio.unibo.it>
2025-08-17 22:25:31 +0200

    feat: more metjods in overworld model + some javadoc

--- src/main/java/it/unibo/progetto_oop/Overworld/MVC/OverworldModel.java
+++ src/main/java/it/unibo/progetto_oop/Overworld/MVC/OverworldModel.java
@@ -90,10 +90,18 @@ public class OverworldModel {
         return this.encounteredEnemy;
     }
 
+    /**
+     * 
+     * @return if the player is in combat
+     */
     public boolean isInCombat(){
         return this.inCombat;
     }
 
+    /**
+     * 
+     * @return if a combat transition is pending
+     */
     public boolean isCombatTransitionPending(){
         return this.combatTransitionPending;
     }
@@ -107,28 +115,57 @@ public class OverworldModel {
         this.encounteredEnemy = encounteredEnemy;
     }
 
-    public void clearInCombatFlag(){
+    /**
+     * Clear the InCombat flag, indicating that the player is no longer in combat.
+     */
+    private void clearInCombatFlag(){
         this.inCombat = false;
     }
 
-    public void setInCombatFlag(){
+    /**
+     * Set the InCombat flag to true, indicating that the player has entered combat.
+     */
+    private void setInCombatFlag(){
         this.inCombat = true;
     }
 
-    public void clearCombatTransitionFlag(){
+    /**
+     * Clear the combat transition flag, indicating that the combat transition is no longer pending.
+     */
+    private void clearCombatTransitionFlag(){
         this.combatTransitionPending = false;
     }
     
-    public void setCombatTransitionFlag(){
+    /**
+     * Set the combat transition flag to true, indicating that a combat transition is pending.
+    */
+    private void setCombatTransitionFlag(){
         this.combatTransitionPending = false;
     }
 
     // methods
 
+    /**
+     * Check if the player has encountered an enemy at the current position
+     * 
+     * @return an Optional containing the enemy if found, otherwise an empty Optional
+     */
     private Optional<Enemy> checkEnemyHit(){
         return this.enemies.stream().filter(enemy -> enemy.getCurrentPosition().equals(this.tempPosition)).findFirst();
     }
 
+    /**
+     * Check if the player has hit a wall at the current position
+     * @return true if the player has hit a wall, false otherwise
+     */
+    private boolean checkWallHit(){
+        return this.walls.contains(this.tempPosition);
+    }
+
+    /**
+     * Remove an enemy from the list of enemies and add it to the list of beaten enemies
+     * @param enemyToRemove
+     */
     private void removeEnemy(Enemy enemyToRemove){
         if (this.enemies.contains(enemyToRemove)){
             this.enemies.remove(enemyToRemove);
@@ -136,8 +173,46 @@ public class OverworldModel {
         }
     }
 
-    private boolean checkWallHit(){
-        return this.walls.contains(this.tempPosition);
+    /**
+     * Remove an item from the overworld and add it to the player's inventory
+     * 
+     * @param item the item to remove
+     */
+    private void removeItem(Item itemToRemove){
+        this.inventory.addItem(itemToRemove);
+        this.items.remove(itemToRemove);
+    }
+
+    /**
+     * Trigger the enemy turns, allowing them to take actions
+     * This method should be called after the player has moved
+     */
+    private void triggerEnemyTurns(){
+        // TODO: for each enemy, check if it can move or attack
+    }
+
+    /**
+     * Check if an item is found at the player's position
+     * @return an Optional containing the item if found, otherwise an empty Optional
+     */
+    private Optional<Item> ItemFoundAtPlayerPosition(){
+        return this.items.stream().filter(
+            item -> item.getPosition().equals(this.player.getPosition())
+            ).findFirst();
+    }
+
+    /**
+     * Check if an item is found at the player's position and add it to the inventory
+     * If an item is found, remove it from the overworld items list
+     */
+    private void checkAndAddItem(){
+        Optional<Item> itemOpt = this.ItemFoundAtPlayerPosition();
+
+        itemOpt.ifPresent(item -> {
+            System.out.println("Item found, picking it up "+item.getName());
+            this.removeItem(item);
+            this.inventory.printInventory();
+        });
     }
 
     /**
@@ -172,9 +247,14 @@ public class OverworldModel {
         // the player can now change position
         this.player.setPosition(tempPosition);
 
-        // TODO: check items
-        // TODO: trigger enemy turn
+        // check items
+        this.checkAndAddItem(); 
+        
+        // trigger enemy turn
+        this.triggerEnemyTurns();
     }
+
+
 
 
 }
