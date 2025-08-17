7154ed65fb91593df2e889c13078be8de1e8b7a7
Matteo Monari <matteo.monari6@studio.unibo.it>
2025-08-17 21:20:58 +0200

    feat: implement PhaseShift strategy with enraged state and attack logic

--- src/main/java/it/unibo/progetto_oop/Combat/PhaseShiftStrategy/PhaseShift.java
+++ src/main/java/it/unibo/progetto_oop/Combat/PhaseShiftStrategy/PhaseShift.java
@@ -1,5 +1,30 @@
 package it.unibo.progetto_oop.Combat.PhaseShiftStrategy;
 
-public class PhaseShift implements  Enemy{
+import it.unibo.progetto_oop.Combat.EnemyStrategyPattern.EnemyAttackStrategy;
+import it.unibo.progetto_oop.Combat.MVC_Pattern.CombatController;
+import it.unibo.progetto_oop.Combat.MVC_Pattern.CombatModel;
+
+public class PhaseShift implements  EnemyAttackStrategy{
+
+    private boolean isEnraged;
+    private CombatModel model;
+
+    @Override
+    public void performAttack(CombatController context) {
+        this.model = context.getModel();
+
+        if(!this.isEnraged && model.getEnemyHealth() <= model.getEnemyHealth()/2){
+
+            this.isEnraged = true;
+            context.getView().showInfo("Enemy is enraged!");
+            context.getModel().increaseEnemyPower(10);
+        }
+        if (!this.isEnraged) {
+            // TODO: Change it to where I can use bigger enemies
+            context.performEnemyPhysicalAttack();
+        } else {
+            context.performEnemySuperAttack();
+        }
+    }
     
 }
