a0341bc30b72dbc946fee1d43dcbf80103bd60e9
Laura Bertozzi <laura.bertozzi5@studio.unibo.it>
2025-08-17 09:10:44 +0200

    feat: started working on overworldController

--- src/main/java/it/unibo/progetto_oop/Overworld/MVC/OverworldController.java
+++ src/main/java/it/unibo/progetto_oop/Overworld/MVC/OverworldController.java
@@ -1,5 +1,22 @@
 package it.unibo.progetto_oop.Overworld.MVC;
 
+import java.util.Objects;
+
+import it.unibo.progetto_oop.Overworld.MVC.*;
+
 public class OverworldController {
-    
+    private final OverworldModel model;
+    private final OverworldView view; 
+    private final OverworldApplication game;
+
+    public OverworldController(OverworldModel model, OverworldView view, OverworldApplication game){
+        this.model = Objects.requireNonNull(model, "Model cannot be null");
+        this.view = Objects.requireNonNull(view, "View cannot be null");
+        this.game = Objects.requireNonNull(game, "Game Application cannot be null");
+    }
+
+    public void initializeImputBindings() {
+        // TODO
+    }
+        
 }
