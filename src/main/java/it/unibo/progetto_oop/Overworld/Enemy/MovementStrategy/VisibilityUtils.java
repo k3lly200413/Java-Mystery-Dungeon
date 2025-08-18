73ad573781e2d4192c736faabf6fdf2f1c0fb85f
Laura Bertozzi <laura.bertozzi5@studio.unibo.it>
2025-08-18 19:05:03 +0200

    feat: idea for the visual range needed for follower enemy

---
+++ src/main/java/it/unibo/progetto_oop/Overworld/Enemy/MovementStrategy/VisibilityUtils.java
@@ -0,0 +1,20 @@
+package it.unibo.progetto_oop.Overworld.Enemy.MovementStrategy;
+
+
+
+/** 
+ * visual range related utilities
+ */
+public class VisibilityUtils {
+
+    // TODO: method to check if player is in enemy visual range
+
+    // TODO: method to calculate the visual range of an enemy --> Bresenham's line algorithm
+
+    // TODO: method to check if there are walls in the way of the enemy's visual range using Bresenham's line algorithm,
+    // so if the player is actually visible to the enemy
+
+    // TODO: method to move the follower towards the player
+
+
+}
