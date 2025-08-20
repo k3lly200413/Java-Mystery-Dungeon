0ee713f057e876e81e49f306fcd43d94ac7dbfbf
Laura Bertozzi <laura.bertozzi5@studio.unibo.it>
2025-08-20 10:51:15 +0200

    feat: ItemFactory

--- src/main/java/it/unibo/progetto_oop/Combat/PotionFactory/ItemFactory.java
+++ src/main/java/it/unibo/progetto_oop/Combat/PotionFactory/ItemFactory.java
@@ -1,5 +1,28 @@
 package it.unibo.progetto_oop.Combat.PotionFactory;
 
+import it.unibo.progetto_oop.Combat.Inventory.Item;
+import it.unibo.progetto_oop.Combat.PotionStrategy.AttackBuff;
+import it.unibo.progetto_oop.Combat.PotionStrategy.CurePoison;
+import it.unibo.progetto_oop.Combat.PotionStrategy.Healing;
+import it.unibo.progetto_oop.Combat.PotionStrategy.Potion;
+
+import it.unibo.progetto_oop.Combat.Position.Position;
+
+/**
+ * @author Laura Bertozzi
+ */
 public class ItemFactory {
-    
+    public Item createItem(String itemId, Position position){
+        if("Health Potion".equalsIgnoreCase(itemId)){
+            return new Potion("Health Potion", "Restores Player Health\nAmount restored: 25", position, new Healing() );
+        }
+        else if ("Antidote".equalsIgnoreCase(itemId)){
+            return new Potion("Antidote", "Cures from poison", position, new CurePoison());
+        }
+        else if ("Attack Buff".equalsIgnoreCase(itemId)){
+            return new Potion("Attack Buff", "Buffs Attack for rest of encounter\nBuff Amount: +10", position, new AttackBuff());
+        }
+        System.err.println("Warning: Unknown item identifier in factory: " + itemId);
+        return null;
+    }
 }
