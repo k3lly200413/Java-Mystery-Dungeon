a47a0f1a0d4625ea99401ba722ccb0f7a333d582
Laura Bertozzi <laura.bertozzi5@studio.unibo.it>
2025-08-27 12:09:53 +0200

    feat: initalized mivement util test

---
+++ src/test/java/it/unibo/progetto_oop/Enemy/MovementUtilTest.java
@@ -0,0 +1,14 @@
+package it.unibo.progetto_oop.Enemy;
+
+import org.junit.jupiter.api.Test;
+
+import it.unibo.progetto_oop.Combat.Position.Position;
+import it.unibo.progetto_oop.Overworld.Enemy.MovementStrategy.MovementUtil;
+
+public class MovementUtilTest {
+
+    @Test
+    public void testGetInitialGeneralMoveDirection(){
+        
+    }
+}
