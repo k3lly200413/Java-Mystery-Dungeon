aca1edc9643d130732e96a19fdc0796095410240
Laura Bertozzi <laura.bertozzi5@studio.unibo.it>
2025-08-28 13:35:34 +0200

    refactor: movement methods in overworld model moved to a separate class

---
+++ src/main/java/it/unibo/progetto_oop/Overworld/MVC/ModelSystem/MovementSystem.java
@@ -0,0 +1,106 @@
+package it.unibo.progetto_oop.Overworld.MVC.ModelSystem;
+
+import java.util.Optional;
+import java.util.Set;
+
+import it.unibo.progetto_oop.Combat.Position.Position;
+import it.unibo.progetto_oop.Overworld.Enemy.CreationPattern.FactoryImpl.Enemy;
+import it.unibo.progetto_oop.Overworld.MVC.OverworldModel;
+import it.unibo.progetto_oop.Overworld.Player.Player;
+
+public class MovementSystem {
+    private final Player player;
+    private final Set<Position> walls; // TODO: integrare con Alice
+    private Position tempPosition;
+    private final OverworldModel model;
+
+    // flags
+    private boolean combatTransitionPending = false;
+    
+
+
+    public MovementSystem(Set<Position> walls, Player player, OverworldModel model) {
+        this.player = player;
+        this.walls = walls;
+        this.model = model;
+    }
+
+
+    // getters
+
+    /**
+     * 
+     * @return if a combat transition is pending
+     */
+    public boolean isCombatTransitionPending(){
+        return this.combatTransitionPending;
+    }
+
+    // setters
+
+    /**
+     * Clear the combat transition flag, indicating that the combat transition is no longer pending.
+     */
+    public void clearCombatTransitionFlag(){
+        this.combatTransitionPending = false;
+    }
+    
+    /**
+     * Set the combat transition flag to true, indicating that a combat transition is pending.
+    */
+    public void setCombatTransitionFlag(){
+        this.combatTransitionPending = true;
+    }
+
+
+
+    /**
+     * Check if the player has hit a wall at the current position
+     * @return true if the player has hit a wall, false otherwise
+     */
+    private boolean checkWallHit(){
+        return this.walls.contains(this.tempPosition);
+    }
+
+
+    /**
+     * Move the player checking if it encounters items, enemies or walls.
+     * If it encounters an enemy transition to combat
+     * 
+     * @param directionX direction of movement on axis x
+     * @param directionY direction of movement on axis y
+     * @param pickupSystem class that handles picking up objects
+     * @param enemySystem class that handles encountered enemies
+     */
+    public void move(int directionX, int directionY, PickupSystem pickupSystem, EnemySystem enemySystem){
+        Position currentPos = player.getPosition();
+        tempPosition = new Position(currentPos.x()+directionX, currentPos.y()+directionY);
+
+        // reset flag and encountered enemy
+        this.clearCombatTransitionFlag();
+        enemySystem.setEncounteredEnemy(null);
+
+        // Check Walls
+        if (checkWallHit()) {
+            this.tempPosition = player.getPosition();
+        }
+    
+        // Check Enemies
+        Optional<Enemy> enemyOpt = enemySystem.checkEnemyHit(tempPosition);
+        if (enemyOpt.isPresent()) {
+            this.setCombatTransitionFlag();
+            enemySystem.setEncounteredEnemy(enemyOpt.get());
+            System.out.println("Enemy encounter flagged at "+tempPosition);
+            return; 
+        }
+
+        // the player can now change position
+        this.player.setPosition(tempPosition);
+
+        // check items
+        pickupSystem.checkAndAddItem(); 
+        
+        // trigger enemy turn
+        enemySystem.triggerEnemyTurns();
+    }
+}
--- src/main/java/it/unibo/progetto_oop/Overworld/MVC/OverworldModel.java
+++ src/main/java/it/unibo/progetto_oop/Overworld/MVC/OverworldModel.java
@@ -2,13 +2,13 @@ package it.unibo.progetto_oop.Overworld.MVC;
 
 import java.util.List;
 import java.util.Set;
-import java.util.Optional;
 
 import it.unibo.progetto_oop.Combat.Inventory.Inventory;
 import it.unibo.progetto_oop.Combat.Inventory.Item;
 import it.unibo.progetto_oop.Overworld.Enemy.CreationPattern.FactoryImpl.Enemy;
 import it.unibo.progetto_oop.Overworld.MVC.ModelSystem.EnemySystem;
 import it.unibo.progetto_oop.Overworld.MVC.ModelSystem.PickupSystem;
+import it.unibo.progetto_oop.Overworld.MVC.ModelSystem.MovementSystem;
 import it.unibo.progetto_oop.Overworld.Player.Player;
 import it.unibo.progetto_oop.Combat.Position.Position;
 
@@ -18,20 +18,20 @@ public class OverworldModel {
     // utility systems
     private final PickupSystem pickupSystem;
     private final EnemySystem enemySystem;
+    private final MovementSystem movementSystem;
 
     private final Set<Position> walls; // TODO: integrare con Alice
 
-    private Position tempPosition;
-
-    private boolean combatTransitionPending = false;
+    // flags
     private boolean inCombat;
 
     public OverworldModel(Player player, List<Enemy> enemies, List<Item> items, Set<Position> walls) { // TODO: integrare con Alice
         this.player = player;
         this.walls = walls;
-        this.inCombat = false;
         this.pickupSystem = new PickupSystem(items, player);
         this.enemySystem = new EnemySystem(enemies, player, this);
+        this.movementSystem = new MovementSystem(walls, player, this);
+        this.inCombat = false;
     }
 
     // --- getter methods ---
@@ -98,7 +98,7 @@ public class OverworldModel {
      * @return if a combat transition is pending
      */
     public boolean isCombatTransitionPending(){
-        return this.combatTransitionPending;
+        return this.movementSystem.isCombatTransitionPending();
     }
 
     // setters
@@ -129,25 +129,17 @@ public class OverworldModel {
      * Clear the combat transition flag, indicating that the combat transition is no longer pending.
      */
     public void clearCombatTransitionFlag(){
-        this.combatTransitionPending = false;
+        this.movementSystem.clearCombatTransitionFlag();
     }
     
     /**
      * Set the combat transition flag to true, indicating that a combat transition is pending.
     */
     public void setCombatTransitionFlag(){
-        this.combatTransitionPending = false;
+        this.setCombatTransitionFlag();
     }
 
     // methods
-
-    /**
-     * Check if the player has hit a wall at the current position
-     * @return true if the player has hit a wall, false otherwise
-     */
-    private boolean checkWallHit(){
-        return this.walls.contains(this.tempPosition);
-    }
 
 
     /**
@@ -158,35 +150,7 @@ public class OverworldModel {
      * @param directionY direction of movement on axis y
      */
     public void movePlayer(int directionX, int directionY){
-        Position currentPos = player.getPosition();
-        tempPosition = new Position(currentPos.x()+directionX, currentPos.y()+directionY);
-
-        // reset flag and encountered enemy
-        this.clearCombatTransitionFlag();
-        this.setEncounteredEnemy(null);
-
-        // Check Walls
-        if (checkWallHit()) {
-            this.tempPosition = player.getPosition();
-        }
-    
-        // Check Enemies
-        Optional<Enemy> enemyOpt = this.enemySystem.checkEnemyHit(tempPosition);
-        if (enemyOpt.isPresent()) {
-            this.setCombatTransitionFlag();
-            this.setEncounteredEnemy(enemyOpt.get());
-            System.out.println("Enemy encounter flagged at "+tempPosition);
-            return; 
-        }
-
-        // the player can now change position
-        this.player.setPosition(tempPosition);
-
-        // check items
-        this.pickupSystem.checkAndAddItem(); 
-        
-        // trigger enemy turn
-        this.enemySystem.triggerEnemyTurns();
+        this.movementSystem.move(directionX, directionY, this.pickupSystem, this.enemySystem);
     }
 
 
