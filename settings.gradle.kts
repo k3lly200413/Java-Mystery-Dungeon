4fdf4f4ac28a6cbb033d347b830c295104589673
Kelly <kelly.applebee@studio.unibo.it>
2025-08-31 16:37:57 +0200

    build: changed gradle settings

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     // Automatically downloads the correct java version to run the static analyzers
     id("org.gradle.toolchains.foojay-resolver-convention") version "1.0.0"
+    id("org.danilopianini.gradle-java-qa") version "1.75.0"
 }
 
 rootProject.name = "sample-gradle-project"
