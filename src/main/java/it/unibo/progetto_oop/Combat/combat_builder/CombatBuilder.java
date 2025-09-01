7a857f9ea0d44b8c536c167ba655d0e006398477
Matteo Monari <matteo.monari6@studio.unibo.it>
2025-09-01 20:00:52 +0200

    docs: add javadoc in combatBuilder

--- src/main/java/it/unibo/progetto_oop/Combat/combat_builder/CombatBuilder.java
+++ src/main/java/it/unibo/progetto_oop/Combat/combat_builder/CombatBuilder.java
@@ -8,18 +8,33 @@ import it.unibo.progetto_oop.combat.mvc_pattern.CombatModel;
  */
 public class CombatBuilder {
 
-    private int newSize;
+    /** The size of the game board or arena. */
+    private int size;
+
+    /** The maximum stamina points of the player. */
     private int staminaMax;
+
+    /** The current attack power of the player. */
     private int playerPower;
+
+    /** The poison attack power of the player. */
     private int playerPoisonPower;
+
+    /** The long-range attack power of the player. */
     private int playerLongRangePower;
+
+    /** The attack power of the enemy. */
     private int enemyPower;
+
+    /** The speed of the enemy. */
     private int enemySpeed;
+
+    /** The name of the enemy. */
     private String enemyName;
 
     /** @return the configured size of the combat area */
     public int getSize() {
-        return newSize;
+        return size;
     }
 
     /** @return the maximum stamina of the player */
@@ -58,63 +73,76 @@ public class CombatBuilder {
     }
 
     /**
-     * Builds a new {@link CombatModel} using the configured parameters.
-     *
-     * @return a new instance of CombatModel
-     */
+    * Builds a new {@link CombatModel} using the configured parameters.
+    *
+    * @return a new instance of CombatModel
+    */
     public CombatModel build() {
         return new CombatModel(this);
     }
 
-    /** @param size the size of the combat area
-    *   @return newSize
+    /** @param newSize the size of the combat area
+    *   @return the size of the combat area
     */
-    public CombatBuilder setSize(final int size) {
-        this.newSize = size;
+    public CombatBuilder setSize(final int newSize) {
+        this.size = newSize;
         return this;
     }
 
-    /** @param staminaMax the maximum stamina of the player */
-    public CombatBuilder setStaminaMax(final int staminaMax) {
-        this.staminaMax = staminaMax;
-        return this;
-    }
-
-    /** @param playerPower the base attack power of the player */
-    public CombatBuilder setPlayerPower(final int playerPower) {
-        this.playerPower = playerPower;
-        return this;
-    }
-
-    /** @param playerPoisonPower the poison attack power of the player */
-    public CombatBuilder setPlayerPoisonPower(final int playerPoisonPower) {
-        this.playerPoisonPower = playerPoisonPower;
-        return this;
-    }
-
-    /** @param playerLongRangePower the long-range attack power of the player */
-    public CombatBuilder setPlayerLongRangePower(final int playerLongRangePower) {
-        this.playerLongRangePower = playerLongRangePower;
-        return this;
-    }
-
-    /** @param enemyPower the attack power of the enemy */
-    public CombatBuilder setEnemyPower(final int enemyPower) {
-        this.enemyPower = enemyPower;
-        return this;
-    }
-
-    /** @param enemySpeed the speed of the enemy
-     * @return this
+    /** @param newStaminaMax the maximum stamina of the player
+    *  @return the maximum stamina of the player
     */
-    public CombatBuilder setEnemySpeed(final int enemySpeed) {
-        this.enemySpeed = enemySpeed;
+    public CombatBuilder setStaminaMax(final int newStaminaMax) {
+        this.staminaMax = newStaminaMax;
         return this;
     }
 
-    /** @param enemyName the name of the enemy */
-    public CombatBuilder setEnemyName(final String enemyName) {
-        this.enemyName = enemyName;
+    /** @param newPlayerPower the base attack power of the player
+    *  @return the base attack power of the player
+    */
+    public CombatBuilder setPlayerPower(final int newPlayerPower) {
+        this.playerPower = newPlayerPower;
+        return this;
+    }
+
+    /** @param newPlayerPoisonPower the poison attack power of the player
+    *  @return the poison attack power of the player
+    */
+    public CombatBuilder setPlayerPoisonPower(final int newPlayerPoisonPower) {
+        this.playerPoisonPower = newPlayerPoisonPower;
+        return this;
+    }
+
+    /** @param newPlayerLongRangePower the long-range attack power of the player
+    *  @return the long-range attack power of the player
+    */
+    public CombatBuilder setPlayerLongRangePower(
+        final int newPlayerLongRangePower) {
+        this.playerLongRangePower = newPlayerLongRangePower;
+        return this;
+    }
+
+    /** @param newEnemyPower the attack power of the enemy
+    *  @return the attack power of the enemy
+    */
+    public CombatBuilder setEnemyPower(final int newEnemyPower) {
+        this.enemyPower = newEnemyPower;
+        return this;
+    }
+
+    /** @param newEnemySpeed the speed of the enemy
+    *   @return the speed of the enemy
+    */
+    public CombatBuilder setEnemySpeed(final int newEnemySpeed) {
+        this.enemySpeed = newEnemySpeed;
+        return this;
+    }
+
+    /** @param newEnemyName the name of the enemy
+    *  @return the name of the enemy
+    */
+    public CombatBuilder setEnemyName(final String newEnemyName) {
+        this.enemyName = newEnemyName;
         return this;
     }
 }
