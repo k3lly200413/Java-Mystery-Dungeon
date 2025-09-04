72c2f033168630c0ccbfbd4a4defe7440cb1667a
Matteo Monari <matteo.monari6@studio.unibo.it>
2025-09-05 00:33:50 +0200

    fix: add newline at end of file and update method signature in attack strategies

---
+++ src/main/java/it/unibo/progetto_oop/Combat/enemy_strategy_pattern/LongRangeAttackStrategy.java
@@ -0,0 +1,12 @@
+package it.unibo.progetto_oop.combat.enemy_strategy_pattern;
+
+import it.unibo.progetto_oop.combat.mvc_pattern.CombatController;
+
+public class LongRangeAttackStrategy implements EnemyAttackStrategy {
+
+    @Override
+    public void performAttack(final CombatController context) {
+
+    }
+
+}
