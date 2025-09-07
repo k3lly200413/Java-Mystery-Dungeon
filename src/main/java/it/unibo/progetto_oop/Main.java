a87ea76aac5dcfcfe434567a5b2ed54117db3cc2
Alice Beccati <alice.beccati@studio.unibo.it>
2025-09-07 15:05:14 +0200

    feat: refactor main application structure by moving OverworldLuncher to a new file and simplifying the main method

---
+++ src/main/java/it/unibo/progetto_oop/Main.java
@@ -0,0 +1,12 @@
+package it.unibo.progetto_oop;
+
+import it.unibo.progetto_oop.Overworld.PlayGround.OverworldLuncher;
+
+public final class Main {
+    private Main() { }
+
+    public static void main(String[] args) {
+        OverworldLuncher app = new OverworldLuncher();
+        app.start();
+    }
+}
