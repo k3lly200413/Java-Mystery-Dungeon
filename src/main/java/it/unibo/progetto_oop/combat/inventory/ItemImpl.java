a0c4d5ceb390d7bad48aa281f5ac4106fe63161e
Laura Bertozzi <laura.bertozzi5@studio.unibo.it>
2025-09-07 09:46:15 +0200

    feat: hash code method in ItemImpl

--- src/main/java/it/unibo/progetto_oop/combat/inventory/ItemImpl.java
+++ src/main/java/it/unibo/progetto_oop/combat/inventory/ItemImpl.java
@@ -1,7 +1,12 @@
 package it.unibo.progetto_oop.combat.inventory;
+import java.util.Objects;
+
 import it.unibo.progetto_oop.Overworld.AdapterPattern.PossibleUser;
 import it.unibo.progetto_oop.Overworld.PlayGround.Data.Position;
 
+/**
+ * @author Laura Bertozzi
+ */
 public abstract class ItemImpl implements Item {
     private final String name;
     private final String description;
@@ -13,17 +18,38 @@ public abstract class ItemImpl implements Item {
         this.position = position;
     }
 
+    @Override
     public String getName() {
         return name;
     }
 
+    @Override
     public String getDescription() {
         return description;
     }
 
+    @Override
     public Position getPosition(){
         return this.position;
     }
 
     public abstract boolean use(PossibleUser target);
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (!(o instanceof ItemImpl)) {
+            return false;
+        }
+        ItemImpl other = (ItemImpl) o;
+        return Objects.equals(name, other.name) &&
+               Objects.equals(description, other.description);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, description);
+    }
 }
