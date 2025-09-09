44ebfc151c7b50856842a814d38e03fa6db2d3c7
Matteo Monari <matteo.monari6@studio.unibo.it>
2025-09-09 11:01:49 +0200

    fix: rename file

--- src/test/java/it/unibo/progetto_oop/combat/helper/NeighboursTest.java
+++ src/test/java/it/unibo/progetto_oop/combat/helper/NeighboursTest.java
@@ -2,7 +2,6 @@ package it.unibo.progetto_oop.combat.helper;
 
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
-
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
