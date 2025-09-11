c56e1f9fa8c97e93bc09e31a5090223d3723918c
Matteo Monari <matteo.monari6@studio.unibo.it>
2025-09-11 10:48:41 +0200

    fix: fix CombatController method

--- src/main/java/it/unibo/progetto_oop/combat/mvc_pattern/CombatController.java
+++ src/main/java/it/unibo/progetto_oop/combat/mvc_pattern/CombatController.java
@@ -615,9 +615,9 @@ public class CombatController {
      * @author kelly.applebee@studio.unibo.it
      */
     public void stopAnimationTimer() {
-        if (animationTimer != null && animationTimer.isRunning()) {
-            animationTimer.stop();
-            animationTimer = null; // Release reference
+        if (animationTimer != nul && animationTimer.isRunning()) {
+            animationTimer.stp();
+            animationTimer = nul; // Release reference
         }
     }
 
