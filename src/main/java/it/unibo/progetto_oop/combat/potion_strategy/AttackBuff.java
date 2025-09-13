3c8bbb98cc77b20a73c3278d05e82e94e78b9ddd
Laura Bertozzi <laura.bertozzi5@studio.unibo.it>
2025-09-13 20:08:21 +0200

    fix : spotbugs

--- src/main/java/it/unibo/progetto_oop/combat/potion_strategy/AttackBuff.java
+++ src/main/java/it/unibo/progetto_oop/combat/potion_strategy/AttackBuff.java
@@ -1,12 +1,14 @@
 package it.unibo.progetto_oop.combat.potion_strategy;
 
+import java.io.Serializable;
+
 import it.unibo.progetto_oop.overworld.player.adapter_pattern.PossibleUser;
 
 /**
  * Class representing a potion strategy that buffs the player's attack power.
  */
 
-public class AttackBuff implements PotionStrategy {
+public class AttackBuff implements PotionStrategy, Serializable {
 
     /** The amount of attack power to buff. */
     private static final int BUFF_AMOUNT = 50;
--- src/main/java/it/unibo/progetto_oop/combat/potion_strategy/CurePoison.java
+++ src/main/java/it/unibo/progetto_oop/combat/potion_strategy/CurePoison.java
@@ -1,12 +1,14 @@
 package it.unibo.progetto_oop.combat.potion_strategy;
 
+import java.io.Serializable;
+
 import it.unibo.progetto_oop.overworld.player.adapter_pattern.PossibleUser;
 
 /**
  * Class representing a potion strategy that cures the player's poison status.
  */
 
-public class CurePoison implements PotionStrategy {
+public class CurePoison implements PotionStrategy, Serializable {
 
     @Override
     public final void applyEffect(final PossibleUser user) {
